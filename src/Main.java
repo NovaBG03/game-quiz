8e570643c9331e3809e1d4c2cf5993d3f997508d
Nikita Koynov <nikicha03@gmail.com>
2023-12-07 13:37:18 +0200

    allow multiple connections to server

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.Writer;
 import java.net.ServerSocket;
 import java.util.Scanner;
 
@@ -12,18 +10,21 @@ public class Main {
 
     public static void startListening(int port) throws IOException {
         try (var server = new ServerSocket(port)) {
-            System.out.println("Waiting for connection...");
-            var socket = server.accept();
-            System.out.println("Client connected " + socket.getInetAddress());
-            var scanner = new Scanner(socket.getInputStream());
-            var writer = new PrintStream(socket.getOutputStream());
-            startGame(scanner, writer);
+            while (true) {
+                System.out.println("Waiting for connection...");
+                var socket = server.accept();
+                System.out.println("Client connected " + socket.getInetAddress());
+                var scanner = new Scanner(socket.getInputStream());
+                var writer = new PrintStream(socket.getOutputStream());
+
+                new Thread(() -> startGame(scanner, writer)).start();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void startGame(Scanner scanner, PrintStream writer) throws IOException {
+    public static void startGame(Scanner scanner, PrintStream writer) {
         writer.println("Добре дошли в играта!");
         writer.println("Въведете име на герой:");
         String heroName = scanner.next();
